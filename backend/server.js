1da4d08d93885397a374c8533be52ef16e1643dd
Ayan Sarkar <ayansarkar2103@example.com>
2026-04-25 02:04:38 +0530

    fix gmail smtp port

--- backend/server.js
+++ backend/server.js
@@ -26,14 +26,13 @@ const razorpay = new Razorpay({
 // EMAIL SETUP
 // -----------------------------
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  port: 587,
-  secure: false, // VERY IMPORTANT
-
 });
 
 transporter.verify((err) => {
